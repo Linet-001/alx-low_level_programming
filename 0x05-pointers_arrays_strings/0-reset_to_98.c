a3b034467f43dd9da1daef7f2a464acfe397587d
Linet-001 <linetmuchunu@gmail.com>
2022-04-20 17:32:29 +0300

    my task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
+#include "main.h"
 
 /**
  *reset_to_98 - updates a integer value to 98
- *@n; A pointer to an int that will be updated
+ *@n: A pointer to an int that will be updated
  *
  *Return: void that means is correct
  */
