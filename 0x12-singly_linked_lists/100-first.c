ae3cbcc66194423306a22057a907f46fce37521f
Linet-001 <linetmuchunu@gmail.com>
2022-05-24 21:01:08 +0300

    my tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
-
-/**
- * bmain - function executed before main
- * Return: no return.
+/*
+ * File: 100-first.c
+ * Author: Muchunu Linet
  */
 
-void __attribute__((constructor)) bmain()
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
